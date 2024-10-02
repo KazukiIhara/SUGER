f63d21a51d5e6cf72b8f1a568bcfcafb8fc71755
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-10-02 11:21:00 +0900

    inputクラス実装完了

---
+++ project/Engine/input/direct/DirectInput.h
@@ -0,0 +1,46 @@
+#pragma once
+// Include
+#define DIRECTINPUT_VERSION	0x0800 // DirectInputのバージョン設定
+#include <dinput.h>
+#include "ComPtr.h"
+
+// pragma comment
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+
+// 前方宣言
+class WindowManager;
+
+// DirectInputクラス
+class DirectInput {
+public:
+	// 初期化
+	void Initialize(WindowManager* windowManager);
+	// 更新
+	void Update();
+
+	// キーの押下をチェック
+	// 押しているとき
+	bool PushKey(BYTE keyNumber);
+	// 押されたとき
+	bool TriggerKey(BYTE keyNumber);
+	// 離したとき
+	bool ReleaseKey(BYTE keyNumber);
+
+private:
+	void SetWindowManager(WindowManager* windowManager);
+
+private:
+
+	// WindowManagerのポインタ
+	WindowManager* windowManager_ = nullptr;
+	// インスタンス
+	ComPtr <IDirectInput8> directInput = nullptr;
+	// キーボード
+	ComPtr <IDirectInputDevice8> keybord = nullptr;
+
+	// 現在フレームのキー入力
+	BYTE key[256];
+	// 1フレーム前のキー入力
+	BYTE preKey[256];
+};
