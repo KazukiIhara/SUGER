4f676ef4cbe2efd8429a09fe7d90ccf5d4d7c087
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-10-21 15:20:09 +0900

    ImGuiのフォントを変更

---
+++ project/Engine/FixFPS/FixFPS.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// C++
+#include <chrono>
+
+class FixFPS {
+public:
+	FixFPS() = default;
+	~FixFPS() = default;
+
+	// 初期化
+	void Initialize();
+	// 更新
+	void Update();
+
+private:
+	// 記録時間
+	std::chrono::steady_clock::time_point reference_;
+	// 目標FPS
+	double targetFPS_ = 60.0;
+};
