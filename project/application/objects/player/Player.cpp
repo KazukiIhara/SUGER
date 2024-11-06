ef50c57319804f14df8ab0a8bcf51d55074fc6d8
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-11-06 12:54:54 +0900

    レティクル読み込み

--- project/application/objects/player/Player.cpp
+++ project/application/objects/player/Player.cpp
@@ -7,6 +7,12 @@ void Player::Initialize() {
 	SUGER::Create3DObject("Player", "Player");
 	// オブジェクト3dコントローラを初期化 
 	player_.Initialize(SUGER::FindObject3D("Player"));
+
+	// 照準作成
+	SUGER::Create2DObject("Reticle", "Reticle");
+	// 照準のコントローラを初期化
+	reticle_.Initialize(SUGER::FindObject2D("Reticle"));
+
 }
 
 void Player::Update() {
