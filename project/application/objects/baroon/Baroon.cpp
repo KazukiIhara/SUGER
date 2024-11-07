3943955a7f18fafc034ea59c9d5539d574c0ecd0
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-11-07 18:04:22 +0900

    風船クラス作成

---
+++ project/application/objects/baroon/Baroon.cpp
@@ -0,0 +1,9 @@
+#include "Baroon.h"
+
+void Baroon::Initialize() {
+
+}
+
+void Baroon::Update() {
+
+}
---
+++ project/application/objects/baroon/Baroon.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Baroon {
+public:
+	Baroon() = default;
+	~Baroon() = default;
+
+	void Initialize();
+
+	void Update();
+
+private:
+
+};
