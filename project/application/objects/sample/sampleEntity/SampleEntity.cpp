e9d0214ed0361b70243625a049248e1e026c5a7b
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-11-13 15:34:26 +0900

    描画対象を変更

--- project/application/objects/sample/sampleEntity/SampleEntity.cpp
+++ project/application/objects/sample/sampleEntity/SampleEntity.cpp
@@ -4,7 +4,7 @@
 
 void SampleEntity::Initialize() {
 	// モデルの読み込みとオブジェクトの作成、コントローラへの登録
-	controller_.Initialize(SUGER::FindObject3D(SUGER::Create3DObject("sampleEntity", "cross Man")));
+	controller_.Initialize(SUGER::FindObject3D(SUGER::Create3DObject("sampleEntity", "boxMan")));
 	// 初期位置を移動
 	controller_.SetTranslate(Vector3(0.0f, 0.0f, 0.0f));
 
