fd41eef10ea98a50ec09d72201172f020f1bb169
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-11-21 15:43:23 +0900

    規定のエミッタークラス変更

--- project/Engine/!FixEngine/emitter/Emitter.cpp
+++ project/Engine/!FixEngine/emitter/Emitter.cpp
@@ -16,16 +16,15 @@ void Emitter::Update() {
 void Emitter::Emit() {
 	// パーティクル発生設定
 	EmitSetting emitSetting{};
+	// 移動量は0
+	emitSetting.velocity = { 0.0f,0.0f,0.0f };
+	// 色は白
+	emitSetting.color = { 1.0f,1.0f,1.0f,1.0f };
 	// 生存時間をひとまず5秒に設定
 	emitSetting.lifeTime = 5.0f;
 	// 発生個数分ループ
 	for (uint32_t i = 0; i < count_; i++) {
-		// 移動量は0
-		emitSetting.velocity = Random::GenerateVector3(-1.0f, 1.0f);
-		// 色は白
-		emitSetting.color = Random::GenerateVector4(0.0f, 1.0f);
-		emitSetting.color.w = 1.0f;
-		particle_->AddNewParticle(GetWorldPosition() + Random::GenerateVector3(-1.0f, 1.0f), emitSetting);
+		particle_->AddNewParticle(GetWorldPosition(), emitSetting);
 	}
 }
 
