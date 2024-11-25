93a7d7ea7bb7b2c21712dff0bf7e71736499d765
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-11-25 15:21:17 +0900

    コライダーマネージャのヘッダー作成

--- project/Engine/manager/collision/CollisionManager.h
+++ project/Engine/manager/collision/CollisionManager.h
@@ -1,10 +1,32 @@
 #pragma once
 
+// C++
+#include <list>
+#include <string>
+
+#include "3d/entityController/EntityController.h"
+
 class CollisionManager {
 public:
 	CollisionManager() = default;
 	~CollisionManager() = default;
 
-private:
+	// 初期化
+	void Initialize();
+	// 更新
+	void Update();
+	// 終了
+	void Finalize();
+	// コライダー追加
+	void AddCollider(EntityController* controller);
+	// コンテナをクリア
+	void ClearContainer();
+	// ペア同士の当たり判定をチェック
+	void CheckCollisionPair(EntityController* controllerA, EntityController controllerB);
+	// すべてのコライダー同士の当たり判定チェック
+	void CheckAllCollisions();
 
+private:
+	// コントローラコンテナ
+	std::list<std::unique_ptr<EntityController>> controllers_;
 };
