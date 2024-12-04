6e2516441fd530b071873d44bd8db5d25c86a103
KazukiIhara <iharakazuki0829@gmail.com>
2024-12-04 23:03:47 +0900

    描画オブジェクトをteapotに戻しました

--- project/application/scene/gameScenes/sample/SampleScene.cpp
+++ project/application/scene/gameScenes/sample/SampleScene.cpp
@@ -43,8 +43,8 @@ void SampleScene::Initialize() {
 	teapotDefaultTransform.translate = { 3.0f,0.0f,0.0f };
 	// teapot作成
 	teapot_ = std::make_unique<Teapot>();
-	teapot_->Initialize(SUGER::CreateEntity("teapot", "walk", teapotDefaultTransform));
-	teapot_->CreateCollider(kNone, kSphere, 0.5f);
+	teapot_->Initialize(SUGER::CreateEntity("teapot", "teapot", teapotDefaultTransform));
+	teapot_->CreateCollider(kNone, kSphere, 1.0f);
 
 	// オブジェクト2Dの作成とコントローラの初期化
 	pronamaChanTex.Initialize(SUGER::Create2DObject("0_pronama_chan", "pronama_chan.png"));
