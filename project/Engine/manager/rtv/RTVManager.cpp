e6128e7e860cf3ad6dcf461910512002dc1c8bee
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-12-10 11:24:11 +0900

    RTVManager作成

--- project/Engine/manager/rtv/RTVManager.cpp
+++ project/Engine/manager/rtv/RTVManager.cpp
@@ -8,11 +8,38 @@
 void RTVManager::Initialize(DXGIManager* dxgi) {
 	// DXGIのセット
 	SetDXGI(dxgi);
+	// デスクリプタヒープの作成
+	descriptorHeap_ = dxgi_->CreateDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_RTV, kMaxRTVCount, false);
+	// デスクリプタ一個分のサイズを取得して記録
+	descriptorSize_ = dxgi_->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+}
 
+uint32_t RTVManager::Allocate() {
+	int index = useIndex;
+	useIndex++;
+	return index;
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE RTVManager::GetDescriptorHandleCPU(uint32_t index) {
+	D3D12_CPU_DESCRIPTOR_HANDLE handleCPU = descriptorHeap_->GetCPUDescriptorHandleForHeapStart();
+	handleCPU.ptr += (descriptorSize_ * index);
+	return handleCPU;
+}
+
+D3D12_GPU_DESCRIPTOR_HANDLE RTVManager::GetDescriptorHandleGPU(uint32_t index) {
+	D3D12_GPU_DESCRIPTOR_HANDLE handleGPU = descriptorHeap_->GetGPUDescriptorHandleForHeapStart();
+	handleGPU.ptr += (descriptorSize_ * index);
+	return handleGPU;
+}
+
+void RTVManager::CreateRTVTexture2d(uint32_t rtvIndex, ID3D12Resource* pResource) {
+	D3D12_RENDER_TARGET_VIEW_DESC rtvDesc;
+	rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;		// 出力結果をSRGBに変換して書き込む
+	rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;	// 2Dテクスチャとして書き込む
+	dxgi_->GetDevice()->CreateRenderTargetView(pResource, &rtvDesc, GetDescriptorHandleCPU(rtvIndex));
 }
 
 void RTVManager::SetDXGI(DXGIManager* dxgi) {
 	assert(dxgi);
 	dxgi_ = dxgi;
 }
-
