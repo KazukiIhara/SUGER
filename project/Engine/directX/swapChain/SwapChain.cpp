31a7b575aa49a1be6b94a0482efec060811c1188
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-12-10 17:12:00 +0900

    スワップチェーンの初期化処理実装

--- project/Engine/directX/swapChain/SwapChain.cpp
+++ project/Engine/directX/swapChain/SwapChain.cpp
@@ -2,16 +2,42 @@
 
 #include <cassert>
 
-void SwapChain::Initialize(DXGIManager* dxgi) {
+#include "directX/dxgi/DXGIManager.h"
+#include "manager/window/WindowManager.h"
+
+void SwapChain::Initialize(WindowManager* windowmanager, DXGIManager* dxgi) {
+	// WindowManagerのインスタンスをセット
+	SetWindowManager(windowmanager);
 	// DXGIのインスタンスをセット
 	SetDXGI(dxgi);
+	// スワップチェーン作成
+	CreateSwapChain();
 }
 
 UINT SwapChain::GetCurrentBuckBufferIndex() {
 	return swapChain_->GetCurrentBackBufferIndex();
 }
 
+void SwapChain::CreateSwapChain() {
+	// スワップチェーンの設定
+	swapChainDesc_.Width = WindowManager::kClientWidth;				// 画面の幅、ウィンドウのクライアント領域を同じものにしておく
+	swapChainDesc_.Height = WindowManager::kClientHeight;				// 画面の高さ、ウィンドウのクライアント領域を同じものしておく
+	swapChainDesc_.Format = DXGI_FORMAT_R8G8B8A8_UNORM;			// 色形式
+	swapChainDesc_.SampleDesc.Count = 1;						// マルチサンプルしない
+	swapChainDesc_.BufferCount = 2;								// ダブルバッファ
+	swapChainDesc_.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;	// モニタにうつしたら、中身を破棄
+	// コマンドキュー、ウィンドウハンドル、設定を渡して生成する
+	hr_ = dxgi_->GetFactory()->CreateSwapChainForHwnd(commandQueue_.Get(), windowManager_->GetHwnd(), &swapChainDesc_,
+		nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(swapChain_.GetAddressOf()));
+	assert(SUCCEEDED(hr_));
+}
+
 void SwapChain::SetDXGI(DXGIManager* dxgi) {
 	assert(dxgi);
 	dxgi_ = dxgi;
+}
+
+void SwapChain::SetWindowManager(WindowManager* windowManager) {
+	assert(windowManager);
+	windowManager_ = windowManager;
 }
--- project/Engine/directX/swapChain/SwapChain.h
+++ project/Engine/directX/swapChain/SwapChain.h
@@ -13,6 +13,8 @@
 #include "directX/includes/ComPtr.h"
 
 class DXGIManager;
+class DirectXCommand;
+class WindowManager;
 
 class SwapChain {
 public:
@@ -20,27 +22,31 @@ public:
 	~SwapChain() = default;
 
 	// 初期化
-	void Initialize(DXGIManager* dxgi);
+	void Initialize(WindowManager* windowmanager, DXGIManager* dxgi);
 
 	// 現在のバックバッファインデックスを取得
 	UINT GetCurrentBuckBufferIndex();
 
 private:
-	// セットをDXGI
+	// スワップチェーン作成
+	void CreateSwapChain();
+	// DXGIをセット
 	void SetDXGI(DXGIManager* dxgi);
-
+	// WindowManagerをセット
+	void SetWindowManager(WindowManager* windowManager);
 private:
-
 	// スワップチェーン
 	ComPtr<IDXGISwapChain4> swapChain_ = nullptr;
-
 	// スワップチェーンリソース
 	ComPtr<ID3D12Resource> swapChainResources_[2] = { nullptr };
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc_{};
 	// RTVリソースのインデックス
 	uint32_t swapchainIndex_[2]{};
-
+	// エラー判別君
+	HRESULT hr_ = S_FALSE;
 private:
 	// dxgiのインスタンスを受け取る箱
-	DXGIManager* dxgi_ = nullptr;
+	DXGIManager* dxgi_ = nullptr; 
+	// WindowManagerのインスタンスを受け取る箱
+	WindowManager* windowManager_ = nullptr;
 };
